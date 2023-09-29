2998a7ed6f4d3b6e8947a148e4ab1fbd63670afa
samueleffiongjacob <samueleffiongjacob@gmail.com>
2023-09-29 06:27:51 +0100

    scope 3

--- PURE JAVASCRIPT/part_4/scopes2.js
+++ PURE JAVASCRIPT/part_4/scopes2.js
@@ -11,3 +11,38 @@ console.log(answer)
 
 var automatedAnswer =
     "your account # is not available" + (isUserValid(false) ? "1234": "not available")
+
+console.log(automatedAnswer)
+
+function condition(){
+    if (isUserValid){
+        return "You may enter";
+    } else {
+        return "ACCESS DIENED"
+    }
+}
+
+var answer2 = condition();
+
+function moveCommand(direction){
+    var whatHappens;
+    switch (direction){
+        case "forward":
+            whatHappens = "you encounter a monster";
+            break
+        case "back":
+            whatHappens = "you arrived home"
+            break
+        case "right":
+            whatHappens = "you found a river"
+            break
+        case "left":
+            whatHappens = "you run into a troll"
+            break
+        default:
+            whatHappens = "please enter a valid direction "
+    }
+    return whatHappens;
+}
+
+console.log(moveCommand("default"))
